49eed3e2caf6fdf72b89535fac1b32b5c0a3aaca
Manuel Castiblanco <71740021+mcastiblanco1251@users.noreply.github.com>
2022-03-01 14:27:19 -0500

    Add files via upload

--- apps/proarroz.py
+++ apps/proarroz.py
@@ -3,8 +3,9 @@ import time
 import pickle
 import numpy as np
 import pandas as pd
+import sklearn
 from sklearn import preprocessing
-#from sklearn.preprocessing import MinMaxScaler 
+#from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 import matplotlib.pyplot as plt
